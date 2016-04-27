4c99d0d1dfd4c7d0b78d4793fe7c3ee743781851
Chris Blackburn <christopher.blackburn@gmail.com>
2016-04-27 08:53:35 +0100

    Golang stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,7 @@
 set -g -x PATH /usr/local/bin $PATH
 set -g -x fish_greeting ''
+set -x GOPATH $HOME/Documents/go
+export PATH=$PATH:$GOPATH/bin
 alias dw="docker-compose run --rm web $ARGV"
 alias dwt="docker-compose run --rm -e RAILS_ENV=test web $ARGV"
 eval (docker-machine env default)
